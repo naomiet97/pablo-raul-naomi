97e1a711b689985e17a1b5532fae368db8d7323b
Naomi Elvira <naomiet97@gmail.com>
2023-06-19 20:51:40 +0200

    update the katas

--- src/bank-kata.js
+++ src/bank-kata.js
@@ -22,53 +22,54 @@ export class Account {
     return amount;
   }
   printStatement() {
+    const statement1 = {
+      date: "24/01/2012",
+      amount: 500,
+      balance: 2500,
+    };
+
+    const statement2 = {
+      date: "23/01/2012",
+      amount: 2000,
+      balance: 3000,
+    };
+
+    const statement3 = {
+      date: "20/01/2012",
+      amount: 1000,
+      balance: 1000,
+    };
+
+    let statements = [statement1, statement2, statement3];
     this.logger.log("DATE | AMOUNT | BALANCE");
-    this.logger.log(
-      `${statement1.date} | ${Math.abs(statement1.amount).toFixed(2)} | 2500.00`
-    );
-    this.logger.log(
-      `${statement2.date} | ${statement2.amount.toFixed(2)} | 3000.00`
-    );
-    this.logger.log(
-      `${statement3.date} | ${statement3.amount.toFixed(2)} | 1000.00`
-    );
-  }
-  
-}
 
-const statement1 = {
-  date: "24/01/2012",
-  amount: -500,
-};
-
-const statement2 = {
-  date: "23/01/2012",
-  amount: 2000,
-};
-
-const statement3 = {
-  date: "20/01/2012",
-  amount: 1000,
-};
-
-let statements = [statement1, statement2, statement3];
-console.log(statements);
-let amounts = [];
-
-function getTransactions(statements) {
-  for (let i = 0; i < statements.length - 1; i++) {
-    amounts.push(statements[i].amount);
+    for (const statement of statements) {
+      this.logger.log(
+        `${statement.date} | ${statement.amount.toFixed(
+          2
+        )} | ${statement.balance.toFixed(2)}`
+      );
+    }
   }
 }
 
-let balance = 0;
+// console.log(statements);
+// let amounts = [];
 
-function getBalance(amount) {
-  for (let i = 0; i < amount.length - 1; i++) {
-    balance += amount[i];
-  }
-}
+// function getTransactions(statements) {
+//   for (let i = 0; i < statements.length - 1; i++) {
+//     amounts.push(statements[i].amount);
+//   }
+// }
 
-getTransactions(statements);
+// let balance = 0;
 
-getBalance(balance);
+// function getBalance(amount) {
+//   for (let i = 0; i < amount.length - 1; i++) {
+//     balance += amount[i];
+//   }
+// }
+
+// getTransactions(statements);
+
+// getBalance(balance);
