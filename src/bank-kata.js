0cd728b0eafce18bc416514686eca685afb11291
Naomi Elvira <naomiet97@gmail.com>
2023-06-19 20:33:05 +0200

    update the kata

--- src/bank-kata.js
+++ src/bank-kata.js
@@ -1,6 +1,19 @@
+/**
+ *
+ *
+ * BALANCE: realizar método p/cálculos y luego convertir a string
+ */
+
+export class Logger {
+  log(message) {
+    console.log(message);
+  }
+}
 export class Account {
   constructor(logger) {
     this.logger = logger;
+    this.statements = [];
+    this.transactions = [];
   }
   deposit(amount) {
     return amount;
@@ -10,11 +23,52 @@ export class Account {
   }
   printStatement() {
     this.logger.log("DATE | AMOUNT | BALANCE");
-    this.logger.log(`24/01/2012 | ${Account.withdraw(amount)}`);
+    this.logger.log(
+      `${statement1.date} | ${Math.abs(statement1.amount).toFixed(2)} | 2500.00`
+    );
+    this.logger.log(
+      `${statement2.date} | ${statement2.amount.toFixed(2)} | 3000.00`
+    );
+    this.logger.log(
+      `${statement3.date} | ${statement3.amount.toFixed(2)} | 1000.00`
+    );
+  }
+  
+}
+
+const statement1 = {
+  date: "24/01/2012",
+  amount: -500,
+};
+
+const statement2 = {
+  date: "23/01/2012",
+  amount: 2000,
+};
+
+const statement3 = {
+  date: "20/01/2012",
+  amount: 1000,
+};
+
+let statements = [statement1, statement2, statement3];
+console.log(statements);
+let amounts = [];
+
+function getTransactions(statements) {
+  for (let i = 0; i < statements.length - 1; i++) {
+    amounts.push(statements[i].amount);
   }
 }
-export class Logger {
-  log(message) {
-    console.log(message);
+
+let balance = 0;
+
+function getBalance(amount) {
+  for (let i = 0; i < amount.length - 1; i++) {
+    balance += amount[i];
   }
 }
+
+getTransactions(statements);
+
+getBalance(balance);
