13052fc071da35d09341e05bf736e87a0c4b4007
Raúl Sendín <raulsendin13@gmail.com>
2023-06-19 19:02:06 +0200

    Changes Account and Logger

--- src/bank-kata.js
+++ src/bank-kata.js
@@ -2,16 +2,17 @@ export class Account {
   constructor(logger) {
     this.logger = logger;
   }
-  deposit(amount) {}
-  withdraw(amount) {}
+  deposit(amount) {
+    return amount;
+  }
+  withdraw(amount) {
+    return amount;
+  }
   printStatement() {
     this.logger.log("DATE | AMOUNT | BALANCE");
-    this.logger.log("24/01/2012 | 500.00 | 2500.00");
-    this.logger.log("23/01/2012 | 2000.00 | 3000.00");
-    this.logger.log("20/01/2012 | 1000.00 | 1000.00");
+    this.logger.log(`24/01/2012 | ${Account.withdraw(amount)}`);
   }
 }
-
 export class Logger {
   log(message) {
     console.log(message);
